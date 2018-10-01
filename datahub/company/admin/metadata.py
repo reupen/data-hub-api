b790eb0706302813163f2072e18f11573780570a
Marco Fucci <info@marcofucci.com>
2018-10-01 17:04:13 +0100

    Convert relative imports to absolute in several apps
    Apps converted:
    - cleanup
    - company
    - core
    - dbmaintainance
    - documents

--- datahub/company/admin/metadata.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from datahub.company.models import ExportExperienceCategory
-from datahub.metadata.admin import DisableableMetadataAdmin
-
-
-admin.site.register(ExportExperienceCategory, DisableableMetadataAdmin)
